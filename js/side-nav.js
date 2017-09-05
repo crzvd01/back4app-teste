fca63e7d5384f2021721384fdd990d465d1db128
Cristian Azevedo <crzvd01@gmail.com>
2017-09-05 12:56:37 -0300

    Add search in header

--- js/side-nav.js
+++ js/side-nav.js
@@ -1,7 +1,7 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "300px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    document.getElementById("header").style.backgroundColor = "rgba(32,138,236,0.4)";
+    document.getElementById("header").style.backgroundColor = "rgba(106, 168, 224,1)";
     document.getElementById("footer1").style.backgroundColor = "rgba(97,97,97,0.4)";
     document.getElementById("footer2").style.backgroundColor = "rgba(34,34,34,0.4)";
  
