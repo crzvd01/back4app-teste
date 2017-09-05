0f1e423c56e977b34641befde8e328a5a8b17873
Cristian Azevedo <crzvd01@gmail.com>
2017-09-05 10:47:29 -0300

    Update css and html files

--- js/navbar-shrink.js
+++ js/navbar-shrink.js
@@ -5,9 +5,11 @@ $(document).ready(function () {
         if ($(document).scrollTop() > 50) {
            $('.navbar-default').addClass('navbar-shrink');
            $('.burger-menu').addClass('burger-shrink');
+           $('.title-header').addClass('title-header-shrink');
         } else {
            $('.navbar-default').removeClass('navbar-shrink');
            $('.burger-menu').removeClass('burger-shrink');
+           $('.title-header').removeClass('title-header-shrink');
         }
 
     });
